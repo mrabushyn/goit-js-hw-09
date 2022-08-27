a28d751bd5d07c48050037c89ce8d76351b53007
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-27 18:18:22 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  bodyTeg: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let intervalId = null;
 
+refs.startBtn.addEventListener('click', onDiscoStart);
+refs.stopBtn.addEventListener('click', onDiscoStop);
+
+function onDiscoStop() {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
+
+function onDiscoStart() {
+  intervalId = setInterval(setColorToStyle, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function setColorToStyle() {
+  refs.bodyTeg.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
